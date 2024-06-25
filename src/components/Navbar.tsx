aad8e6f12390b1607fe07b4c982473658c77f727
Kaustubh <kaustubhagrawal8838@gmail.com>
2024-06-25 13:03:01 +0530

    debounce

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,33 +1,54 @@
 'use client';
 
 import { useSearchContext } from '@/context/searchContext';
-import { ChangeEventHandler } from 'react';
+import { ChangeEventHandler, useEffect, useState } from 'react';
+import { useDebounce } from 'use-debounce';
 
 export default function Navbar() {
-  const setQueryParam = useSearchContext(d => d.setQuery)
+	const setQueryParam = useSearchContext((d) => d.setQuery);
+	const [query, setQuery] = useState('');
+	const [value] = useDebounce(query, 1000);
 
-  const handleInputChange: ChangeEventHandler<HTMLInputElement> = (e) => {
-    setQueryParam(e.target.value)
-  }
+	useEffect(() => {
+		setQueryParam(value);
+	}, [setQueryParam, value]);
 
-  return (
-<nav className="bg-white border-gray-200 dark:bg-gray-900">
-  <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-    <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Kaustubh</span>
-        
-        <div className="flex flex-col md:flex-row md:order-2 pt-4 md:pt-0">
-          <div className="relative">
-      <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-        <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-        </svg>
-        <span className="sr-only">Search icon</span>
-      </div>
-      <input type="text" id="search-navbar" className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search..." onChange={handleInputChange} />
-          </div>
-          
-        </div>
-  </div>
-</nav>
-  )
+	return (
+		<nav className='bg-white border-gray-200 dark:bg-gray-900'>
+			<div className='max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4'>
+				<span className='self-center text-2xl font-semibold whitespace-nowrap dark:text-white'>
+					Kaustubh
+				</span>
+
+				<div className='flex flex-col md:flex-row md:order-2 pt-4 md:pt-0'>
+					<div className='relative'>
+						<div className='absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none'>
+							<svg
+								className='w-4 h-4 text-gray-500 dark:text-gray-400'
+								aria-hidden='true'
+								xmlns='http://www.w3.org/2000/svg'
+								fill='none'
+								viewBox='0 0 20 20'>
+								<path
+									stroke='currentColor'
+									stroke-linecap='round'
+									stroke-linejoin='round'
+									stroke-width='2'
+									d='m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z'
+								/>
+							</svg>
+							<span className='sr-only'>Search icon</span>
+						</div>
+						<input
+							type='text'
+							id='search-navbar'
+							className='block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+							placeholder='Search...'
+							onChange={(e) => setQuery(e.target.value)}
+						/>
+					</div>
+				</div>
+			</div>
+		</nav>
+	);
 }
